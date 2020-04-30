cb6ba13ce48fea68afdf8faf198262ac62acc513
channa006 <59229989+channa006@users.noreply.github.com>
2020-04-30 11:18:33 +0530

    After Inter fix

--- lists.py
+++
@@ -1,8 +0,0 @@
-dict = {"Anna":"Vanilla","George":"Scotch","Gary":"Rum"}
-
-
-questions = list(dict.keys())
-
-# print(questions)
-
-print(dict["Anna"])
--- rte.py
+++
@@ -1,3 +0,0 @@
-import netmiko
-
-dir(netmiko)
